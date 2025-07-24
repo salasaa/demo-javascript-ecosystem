be9d104c6432d2b2c43890c821b13a52c4e41ab7
salasaa <akhirudinsalasa.gc@gmail.com>
2025-07-25 00:33:08 +0700

    feat: initialize Vite project with TypeScript

---
+++ vite-bun/src/counter.ts
@@ -0,0 +1,13 @@
+export function add(a: number, b: number) {
+  return a + b;
+}
+
+export function setupCounter(element: HTMLButtonElement) {
+  let counter = 0;
+  const setCounter = (count: number) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
+}
