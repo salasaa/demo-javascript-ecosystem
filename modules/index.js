8bb3ba1c33e187c92e03e762a42cc2784eba4bca
salasaa <akhirudinsalasa.gc@gmail.com>
2025-07-24 20:26:10 +0700

    feat: demo js modules

---
+++ modules/index.js
@@ -0,0 +1,5 @@
+import { add, subtract } from "./lib.js";
+
+console.log("66 + 55 + 33 =", add(66, 55, 33)); // 5
+
+console.log("154 - 55 =", subtract(154, 55));
---
+++ modules/lib.js
@@ -0,0 +1,7 @@
+export function add(a, b, c) {
+  return a + b + c;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
